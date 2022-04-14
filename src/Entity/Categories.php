0b828976ffc5ccf5dcd27727a3624be0fc7b8933
trabrouss1 <aboudramane97@gmail.com>
2022-04-14 23:49:32 +0000

    Creation des controllers

--- src/Entity/Categories.php
+++ src/Entity/Categories.php
@@ -22,6 +22,9 @@ class Categories
     #[ORM\Column(type: 'string', length: 100)]
     private $name;
 
+    #[ORM\Column(type: 'integer')]
+    private $categoryOrder;
+
     #[ORM\ManyToOne(targetEntity: self::class, inversedBy: 'categories')]
     #[ORM\JoinColumn(onDelete: "CASCADE")]
     private $parent;
@@ -51,6 +54,18 @@ class Categories
     public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCategoryOrder(): ?int
+    {
+        return $this->categoryOrder;
+    }
+
+    public function setCategoryOrder(int $categoryOrder): self
+    {
+        $this->categoryOrder = $categoryOrder;
 
         return $this;
     }
